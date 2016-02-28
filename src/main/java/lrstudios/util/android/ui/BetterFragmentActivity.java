b24de37e32afaef94b6852c4ae5dcd3ec0111086
Dalmas <emmanuel.mathis@gmail.com>
2016-02-28 11:51:05 +0100

    Added partial chinese (simplified) translation.

--- src/main/java/lrstudios/util/android/ui/BetterFragmentActivity.java
+++ src/main/java/lrstudios/util/android/ui/BetterFragmentActivity.java
@@ -39,11 +39,20 @@ public class BetterFragmentActivity extends AppCompatActivity {
     private ProgressBar _progressIndicator;
 
 
+    protected boolean useToolbar() {
+        return false;
+    }
+
     @Override
     public void setContentView(int layoutResID) {
         super.setContentView(layoutResID);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        if (toolbar != null) {
+            if (useToolbar())
+                setSupportActionBar(toolbar);
+            else
+                toolbar.setVisibility(View.GONE);
+        }
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         _progressIndicator = (ProgressBar) findViewById(R.id.progress_indicator);
